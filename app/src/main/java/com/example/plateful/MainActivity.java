12fc71323f52cf5590a5220ad6627e985ee16230
salmanmirr <hanranakuma@gmail.com>
2024-12-25 04:22:16 +0600

    finished with the random recipe calling. need to fix the card size

--- app/src/main/java/com/example/plateful/MainActivity.java
+++ app/src/main/java/com/example/plateful/MainActivity.java
@@ -1,19 +1,61 @@
 package com.example.plateful;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.plateful.Adapters.RandomRecipeAdapter;
+import com.example.plateful.Listeners.RandomRecipeResponseListeners;
+import com.example.plateful.Models.RandomRecipeApiResponse;
 
 public class MainActivity extends AppCompatActivity {
+    ProgressDialog dialog;
+    RequestManager manager;
+    RandomRecipeAdapter randomRecipeAdapter;
+    RecyclerView recyclerView;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        dialog = new ProgressDialog(this);
+        dialog.setTitle("Loading...");
+
+        manager = new RequestManager(this);
+        manager.getRandomRecipes(randomRecipeResponseListeners);
+        dialog.show();
     }
+
+    private final RandomRecipeResponseListeners randomRecipeResponseListeners = new RandomRecipeResponseListeners() {
+        @Override
+        public void didFetch(RandomRecipeApiResponse response, String message) {
+            dialog.dismiss();
+            recyclerView = findViewById(R.id.recycler_random);
+            recyclerView.setHasFixedSize(true);
+            recyclerView.setLayoutManager(new GridLayoutManager(MainActivity.this, 1));
+            randomRecipeAdapter = new RandomRecipeAdapter(MainActivity.this, response.recipes);
+            recyclerView.setAdapter(randomRecipeAdapter);
+
+
+
+        }
+
+        @Override
+        public void didError(String message) {
+            Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT);
+
+        }
+    };
 }
