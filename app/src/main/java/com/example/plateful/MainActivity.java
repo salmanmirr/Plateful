e53a481cc0effca1fb584c29796ac36f3c89ba5a
Hridoy Sarkar <kalamkhan98325789@gmail.com>
2024-12-25 20:40:44 +0600

    add the search feature to the app

--- app/src/main/java/com/example/plateful/MainActivity.java
+++ app/src/main/java/com/example/plateful/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -30,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     Spinner spinner;
     List<String> tags = new ArrayList<>();
+    SearchView searchView;
 
 
 
@@ -41,6 +43,22 @@ public class MainActivity extends AppCompatActivity {
 
         dialog = new ProgressDialog(this);
         dialog.setTitle("Loading...");
+        searchView = findViewById(R.id.searchView_home);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                tags.clear();
+                tags.add(query);
+                manager.getRandomRecipes(randomRecipeResponseListeners,tags);
+                dialog.show();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
         spinner = findViewById(R.id.apinner_tags);
         ArrayAdapter arrayAdapter =ArrayAdapter.createFromResource(
                 this,
